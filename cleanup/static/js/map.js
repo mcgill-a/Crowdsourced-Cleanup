7865cb4b4d40a872434d27ae689df955b3e446de
rossco906 <s1424268@ed.ac.uk>
2019-09-11 16:11:34 +0100

    added remove markers

--- cleanup/static/js/map.js
+++ cleanup/static/js/map.js
@@ -1,5 +1,7 @@
 var gmap, pos, infowindow;
-
+var markers = []; 
+var counter = 0;
+var openedMarkerID;
 function initMap() {
     gmap = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -9,14 +11,17 @@ function initMap() {
         zoom: 15
     });
     infowindow = new google.maps.InfoWindow({
-        content: '<img src="/static/resources/default/trash-icon.jpg"/>' +
-            '<div>' +
+        content: '<div>' +
             '<ul>' +
             '<li>Detail 1</li>' +
             '<li>Detail 2</li>' +
             '<li>Detail 3</li>' +
             '</ul>' +
-            '</div>'
+            '</div>' +
+            '<div>' +
+            '<img src="/static/resources/default/trash-icon.jpg"/>' +
+            '</div>'+
+            '<button onclick="removemarker()" class="btnSmall">Remove</button'
     });
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -36,10 +41,17 @@ function addMarker() {
         map: gmap,
         animation: google.maps.Animation.DROP,
         title: "Trash #00000",
-        icon: '/static/resources/default/markers/red_markerA.png'
+        icon: '/static/resources/default/markers/red_markerA.png',
+        id: counter
     });
     marker.addListener('click', function () {
         infowindow.open(gmap, marker);
+    })
+    markers.push(marker)
+    counter++;
+    marker.addListener('click', function () {
+        infowindow.open(gmap, marker);
+        openedMarkerID = marker.id;
     })
 }
 
@@ -53,11 +65,19 @@ function addRandomMarker(colour) {
     var marker = new google.maps.Marker({
         map: gmap,
         position: newPos,
-        icon: '/static/resources/default/markers/' + colour
+        icon: '/static/resources/default/markers/' + colour,
+        id: counter
 
     });
-
+    markers.push(marker)
+    counter++;
     marker.addListener('click', function () {
         infowindow.open(gmap, marker);
+        openedMarkerID = marker.id;
     })
+}
+
+function removemarker(){
+    var marker = markers[openedMarkerID];
+    marker.setMap(null);
 }
