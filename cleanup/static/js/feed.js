f352149fafd97a5f9539253b1825962446d2b596
Ross <ross.s.cockburn@gmail.com>
2019-09-13 05:47:49 +0100

    pin pans for cleaned marker

--- cleanup/static/js/feed.js
+++ cleanup/static/js/feed.js
@@ -13,7 +13,7 @@ function refresh () {
                 resHTML += "<tr class='feedTR'><td class='feedEntry'>New <div class='clickable' onClick='goToPin(\""+element.incident_id+"\")'>pin</div> uploaded by <div class='clickable' onClick='window.location.href = \"/users?user=" + element.user_id + "\";'>" + element.user_first_name + "</div></td></tr>";
             }
             else if (element.type == "clean") {
-                resHTML += "<tr class='feedTR'><td class='feedEntry'><div class='clickable' onClick='window.location.href = \"/?pin=" + element.incident_id + "\";'>pin</div> was cleaned by <div class='clickable' onClick='window.location.href = \"/users?user=" + element.user_id + "\";'>" + element.user_first_name + "</div></td></tr>";
+                resHTML += "<tr class='feedTR'><td class='feedEntry'><div class='clickable' onClick='goToPin(\""+element.incident_id+"\")'>pin</div> was cleaned by <div class='clickable' onClick='window.location.href = \"/users?user=" + element.user_id + "\";'>" + element.user_first_name + "</div></td></tr>";
             }
         });
         $('#feed').html(resHTML);
